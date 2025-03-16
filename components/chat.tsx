ee7ebef002ef7de9a9b11abbd3322bec5d521582
RadyaI <radyaiftikhar@gmail.com>
2025-03-16 14:29:41 +0700

    Deploy

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,10 +1,17 @@
 "use client"
 
 import { db } from "@/config/firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import React, { useEffect, useState } from "react"
+import { addDoc, collection, getDocs, onSnapshot, orderBy, query, Timestamp, where } from "firebase/firestore";
+import React, { useCallback, useEffect, useState } from "react"
 import styled, { keyframes } from "styled-components";
 import { FcLock } from "react-icons/fc";
+
+type ChatMessage = {
+    id: string,
+    roomCode: string
+    message: string,
+    createdAt: number
+} | null
 
 export default function Chat({ roomId }: { roomId: string }) {
 
@@ -13,6 +20,38 @@ export default function Chat({ roomId }: { roomId: string }) {
     const [inputPassword, setInputPassword] = useState<string>("");
     const [exp, setExp] = useState<number>(0);
     const [createdAt, setCreatedAt] = useState<number>(0);
+    const [inputMessage, setInputMessage] = useState<string>("");
+    const [chatData, setChatData] = useState<ChatMessage[] | null>([null]);
+
+    const getChatData = useCallback(() => {
+        try {
+            return onSnapshot(query(
+                collection(db, "chats"),
+                where("roomCode", "==", roomId),
+                orderBy("createdAt", "desc")
+            ), (snapshot) => {
+                const temp: ChatMessage[] = [];
+                snapshot.forEach((data) => {
+                    temp.push({ ...data.data(), id: data.id } as ChatMessage)
+                })
+                setChatData(temp);
+            });
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.log(error.message);
+            }
+        }
+    }, [roomId]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (Timestamp.now().toMillis() > exp) {
+                setStatus("notfound");
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [exp])
 
     useEffect(() => {
         async function checkRoom() {
@@ -40,7 +79,8 @@ export default function Chat({ roomId }: { roomId: string }) {
         }
 
         checkRoom()
-    }, [])
+        getChatData()
+    }, [roomId, getChatData])
 
     if (status === "loading") {
         return (
@@ -88,6 +128,30 @@ export default function Chat({ roomId }: { roomId: string }) {
         )
     }
 
+    async function submitMessage(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === "Enter") {
+            try {
+                if (inputMessage.trim()) {
+                    const data = {
+                        roomCode: roomId,
+                        message: inputMessage,
+                        createdAt: Timestamp.now().toMillis()
+                    }
+
+                    setInputMessage("")
+                    await addDoc(collection(db, "chats"), data)
+                } else {
+                    alert("Empty message!")
+                }
+
+            } catch (error: unknown) {
+                if (error instanceof Error) {
+                    console.log(error.message)
+                }
+            }
+        }
+    }
+
     return (
         <>
             <div className="w-[90%]  sm:w-1/2 h-[95dvh] mx-auto mt-4">
@@ -109,50 +173,28 @@ export default function Chat({ roomId }: { roomId: string }) {
                     minute: '2-digit'
                 })}</div>
 
-                <ChatWrapper className="w-[90%] px-4 mx-auto flex flex-col items-end h-[85%] mt-6">
-                    
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Hellowwww</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    <div className="text-right gap-2 w-full mt-6">
-                        <p>Lorem</p>
-                        <p className="text-[10px]">20:01</p>
-                    </div>
-                    
+                <ChatWrapper className="w-[90%] h-[75%] px-4 mx-auto flex flex-col items-end mt-6">
+
+                    {chatData?.map((i, index) =>
+                        <div key={index} className="text-right gap-2 w-full mt-6">
+                            <p>{i?.message}</p>
+                            <p className="text-[10px]">{new Date(i?.createdAt ?? 0).toLocaleString("id-ID",
+                                {
+                                    hour12: false,
+                                    day: "2-digit",
+                                    month: "short",
+                                    year: "numeric",
+                                    hour: "2-digit",
+                                    minute: '2-digit'
+                                }
+                            )}</p>
+                        </div>
+                    )}
+
                 </ChatWrapper>
+                <div className="mx-auto w-[90%]">
+                    <input type="text" onKeyUp={(e) => submitMessage(e)} value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} placeholder="Message..." className="w-full px-3 py-2 mt-3 outline-none" />
+                </div>
             </div>
         </>
     )
@@ -190,8 +232,9 @@ const ChatWrapper = styled.div`
     }
 
     &::-webkit-scrollbar-thumb{
-        width: 5px;
+        /* width: 5px;
         background-color: #efefef;
-        border-radius: 50px;
+        border-radius: 50px; */
+        display: none;
     }
 `
